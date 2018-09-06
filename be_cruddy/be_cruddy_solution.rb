28e81bae7df705ab72bd002868afd2af9a44f35b
Tatiana Pena <Tatiana.c.pena1@gmail.com>
2018-09-06 11:05:30 -0500

    09.05.18

--- be_cruddy/be_cruddy_solution.rb
+++ be_cruddy/be_cruddy_solution.rb
@@ -1,10 +1,20 @@
 # Warmup
 
+speaker = Speakers.last("Hendrickson")
+speaker = Speakers.select(id: 4)
+speaker = Speakers.all
+
 # 1
+rails new conference_app 
+cd conference_app
+subl .
 
 # 2
+rails g model Speaker first_name:string last_name:string email:string
 
 # 3
-
+speaker = Speaker.new(first_name: "Sally", last_name: "Johnson", email: "sallyj@gmail.com")
+speaker.save
 
 # Paste repository link here:
+https://github.com/tapena/conference_app.git
